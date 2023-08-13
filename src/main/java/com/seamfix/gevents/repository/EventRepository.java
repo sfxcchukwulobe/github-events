0b38a474efbd19948483ca1326aace2f7b2024db
Chukwuemeka Chukwulobe <cchukwulobe@seamfix.com>
2023-08-13 13:45:11 +0100

    Initial commit

---
+++ src/main/java/com/seamfix/gevents/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.seamfix.gevents.repository;
+
+import com.seamfix.gevents.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
